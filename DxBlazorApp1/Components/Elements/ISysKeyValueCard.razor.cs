8b79d8bcee970c41157f754ec015fce07b256ad5
Ivano Modenin <i.modenin@axxelera.it>
2024-09-20 09:55:25 +0200

    KeyValueCard

---
+++ DxBlazorApp1/Components/Elements/ISysKeyValueCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DxBlazorApp1.Components.Elements
+{
+
+    /// <summary>
+    /// Card orizzontale che visualizza coppia di valori affiancati, con l'ultimo valore separato da barra verticale
+    /// </summary>
+    public partial class ISysKeyValueCard
+    {
+        [Parameter] public IEnumerable<KeyValuePair<string, string>> Valori { get; set; } = [];
+
+    }
+}
