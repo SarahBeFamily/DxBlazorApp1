993d7ab0627b1b8a5d2f00b3b696628997fef805
babyperrito <babyperrito@gmail.com>
2024-09-26 09:48:30 +0200

    Fix visualizzazione tabella, pager, aggiunto componente bottone custom

---
+++ DxBlazorApp1/Components/Elements/ISysBtn.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DxBlazorApp1.Components.Elements
+{
+	public partial class ISysBtn
+	{
+		[Parameter] public string Text { get; set; }
+		[Parameter] public string CssClass { get; set; }
+		[Parameter] public string RenderStyle { get; set; }
+		[Parameter] public EventCallback OnClick { get; set; }
+	}
+}
