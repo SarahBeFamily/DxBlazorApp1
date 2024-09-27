ff0330a557140af1acc0736de4d600a40a89f99d
babyperrito <babyperrito@gmail.com>
2024-09-27 16:09:45 +0200

    Impostazione tabs in pagina Archivio Richieste

--- DxBlazorApp1/Services/MenuItems.cs
+++ DxBlazorApp1/Services/MenuItems.cs
@@ -8,12 +8,12 @@ namespace DxBlazorApp1.Services
         private static readonly Lazy<List<MenuItemGroup>> menuItemGroups = new(() =>
         {
             List<MenuItemGroup> groups = [
-                new MenuItemGroup("Dashboard", "icon-nav icon-dashboard", "images/Home.svg", "/"),
+                new MenuItemGroup("Dashboard", "icon-nav icon-dashboard", "images/Home.svg", "dashboard"),
                 new MenuItemGroup("Logistica", "icon-nav icon-logistica", "images/Logistica.svg", "#",
                      [
                         new("Richiesta ritiro", "", "", "richiesta-ritiro"),
                         new("Richieste di trattamento", "", "", "#"),
-                        new("Archivio richieste", "", "", "#"),
+                        new("Archivio richieste", "", "", "archivio-richieste"),
                      ]),
                 new MenuItemGroup("Documenti", "icon-nav icon-doc", "images/Documenti.svg", "#",
                      [
