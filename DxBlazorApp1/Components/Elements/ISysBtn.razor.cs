61f47eb56d1eedbf556bddb1bb51e92b0842bd7f
babyperrito <babyperrito@gmail.com>
2024-10-04 13:24:27 +0200

    Fix e modifiche richieste su tabelle, menù e disposizione delle card. Aggiunte nuove classi in card per la gestione dell'altezza e della loro posizione. Modifcato ISysBtn con l'aggiunta dell'icona (tramite classe) e il rendermode primary.

--- DxBlazorApp1/Components/Elements/ISysBtn.razor.cs
+++ DxBlazorApp1/Components/Elements/ISysBtn.razor.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace DxBlazorApp1.Components.Elements
 {
 	public partial class ISysBtn
 	{
-		[Parameter] public string Text { get; set; }
-		[Parameter] public string CssClass { get; set; }
-		[Parameter] public string RenderStyle { get; set; }
+		[Parameter] public string? Text { get; set; }
+		[Parameter] public string? CssClass { get; set; }
+        [Parameter] public string? Icon { get; set; }
+        [Parameter] public required string RenderStyle { get; set; }
 		[Parameter] public EventCallback OnClick { get; set; }
-	}
+    }
 }
