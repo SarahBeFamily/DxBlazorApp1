574822700b9de97f0f20b6159ab0b21d90212163
Ivano Modenin <i.modenin@axxelera.it>
2024-10-18 13:54:19 +0200

    Pagina upload fir

---
+++ DxBlazorApp1/Components/Elements/ISysToolbar.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DxBlazorApp1.Components.Elements
+{
+    public partial class ISysToolbar
+    {
+
+        /// <summary>
+        /// Lista dei bottoni da mostrare
+        /// </summary>
+        [Parameter] public List<ToolbarItem> ToolbarItems { get; set; } = new();
+
+        /// <summary>
+        /// Notifica quando un bottone viene premuto
+        /// </summary>
+        [Parameter] public EventCallback<ToolbarItem> ItemClicked { get; set; }
+
+        /// <summary>
+        /// Indica se mostrare la casella di ricerca
+        /// </summary>
+        [Parameter] public bool ShowSearchBox { get; set; }
+
+        /// <summary>
+        /// Notifica la modifica del testo di ricerca
+        /// </summary>
+        [Parameter] public EventCallback<string> OnTextChanged { get; set; }
+
+        /// <summary>
+        /// Testo immesso nella casella di ricerca
+        /// </summary>
+        protected string? SearchText { get; set; }
+    }
+}
---
+++ DxBlazorApp1/Components/Elements/ToolBarItemHelper.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.Blazor;
+
+namespace DxBlazorApp1.Components.Elements
+{
+    /// <summary>
+    /// Bottoni standard da aggiungere alla Toolbar
+    /// </summary>
+    public static class ToolbarItemHelper
+    {
+        public const string AddKey = "add";
+        public const string SaveKey = "save";
+        public const string BackKey = "back";
+        public const string CloseKey = "close";
+        public const string DeleteKey = "delete";
+        public const string CsvKey = "csv";
+        public const string XlsKey = "xls";
+
+
+        public static ToolbarItem AddButton() => new()
+        {
+            Icon = "oi oi-plus",
+            Alignment = ToolbarItemAlignment.Left,
+            Key = AddKey,
+        };
+
+        public static ToolbarItem SaveButton() => new()
+        {
+            Icon = "oi oi-circle-check",
+            Alignment = ToolbarItemAlignment.Right,
+            Key = SaveKey,
+            SubmitForm = true
+        };
+
+        public static ToolbarItem BackButton() => new()
+        {
+            Icon = "oi oi-action-undo",
+            Alignment = ToolbarItemAlignment.Left,
+            Key = BackKey,
+        };
+
+        public static ToolbarItem CloseButton() => new()
+        {
+            Icon = "oi oi-x",
+            Alignment = ToolbarItemAlignment.Right,
+            Key = CloseKey,
+        };
+
+        public static ToolbarItem DeleteButton() => new()
+        {
+            Icon = "oi oi-trash",
+            Alignment = ToolbarItemAlignment.Right,
+            Key = DeleteKey,
+        };
+
+        public static ToolbarItem ExpCsvButton() => new()
+        {
+            Icon = "oi oi-file",
+            Alignment = ToolbarItemAlignment.Right,
+            Key = CsvKey,
+        };
+        public static ToolbarItem ExpXlsButton() => new()
+        {
+            Icon = "oi oi-spreadsheet",
+            Alignment = ToolbarItemAlignment.Right,
+            Key = XlsKey,
+        };
+
+    }
+}
