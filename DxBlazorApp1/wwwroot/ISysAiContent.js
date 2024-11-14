ec50cb0ac4503a22d950085209c0a68f9eaaa74e
babyperrito <babyperrito@gmail.com>
2024-11-14 11:07:14 +0100

    Sistemato layout iframe assistenza, e modificato componente con badeg per AI (Nota: il javascript non carica ancora dentro il popup)

---
+++ DxBlazorApp1/wwwroot/ISysAiContent.js
@@ -0,0 +1,11 @@
+﻿export function renderAI() {
+
+        document.querySelectorAll(".isys-ai-content").forEach(function (item) {
+            var aiText = item.getAttribute("data-ai");
+            var aiResult = item.getAttribute("data-ai-result");
+            var label = item.querySelector("label");
+
+            label.classList.add(aiResult);
+            label.setAttribute("data-ai", aiText);
+        });
+}
