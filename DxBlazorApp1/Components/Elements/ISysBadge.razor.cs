b38def4e9f97928664564255dec7bd46fb99742d
Ivano Modenin <i.modenin@axxelera.it>
2025-01-29 14:35:36 +0100

    Badge

--- DxBlazorApp1/Components/Elements/ISysBadge.razor.cs
+++ DxBlazorApp1/Components/Elements/ISysBadge.razor.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Components;
 
-/// <summary>
-/// Badge di stato per tabella. Scegliere tra i colori: gray, green, red, orange, yellow e black
-/// </summary>
+
 
 namespace DxBlazorApp1.Components.Elements
 {
-	public partial class ISysBadge
-	{
-		[Parameter] public required string Label { get; set; }
-		[Parameter] public string? Color { get; set; }
-		[Parameter] public bool ColorGray { get; set; } = false;
-		[Parameter] public bool ColorGreen { get; set; } = false;
-		[Parameter] public bool ColorRed { get; set; } = false;
-		[Parameter] public bool ColorYellow { get; set; } = false;
-		[Parameter] public bool ColorOrange { get; set; } = false;
-		[Parameter] public bool ColorBlack { get; set; } = false;
-	}
+    public partial class ISysBadge
+    {
+        [Parameter, EditorRequired] public string? Label { get; set; } = "";
+        [Parameter] public string? Color { get; set; } = Gray;
+
+
+        public static string Gray = "gray";
+        public static string Green = "green";
+        public static string Red = "red";
+        public static string Yellow = "yellow";
+        public static string Orange = "orange";
+        public static string Black = "black";
+        public static string White = "white";
+    }
 }
